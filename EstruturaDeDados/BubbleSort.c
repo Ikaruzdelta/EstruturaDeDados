fa374536b7fa299680a48039783a95b7d99a086d
Ikaruz <58690965+Ikaruzdelta@users.noreply.github.com>
2021-03-30 16:20:10 -0300

    BubbleSort
    Neste comite eu entrego um código como exemplo de BubbleSort

---
+++ EstruturaDeDados/BubbleSort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define TAM 10
+
+/*
+    "BubbleSort" ou "Ordenação por bolha"
+
+    - Compara pares de elementos adjacentes e
+    os troca de lugar se estiverem na ordem errada
+
+    - Esse Processo se repete até que mais nenhumma
+    troca seja necessaria (elementos já ordenados)
+
+    "Performace"
+    - Melhor caso: O(N)
+    - Pior caso: O(N^2)
+    - Não recomenda para grandes conjuntos de dados
+*/
+//Aqui eu estou testando o envio
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int numeros[TAM];
+    int i, aux, contador;
+
+    printf("Entre com dez números para preencher o array e pressione enter após digitar cada um:\n");
+    for(i = 0; i < TAM; i++)
+    {
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("Ordem atual dos numeros na array:\n");
+    for(i = 0; i < TAM; i++)
+    {
+        printf("%3d", numeros[i]);
+    }
+
+    //Algoritmo de ordenação BubbleSort:
+    for(contador = 1; contador < TAM; contador++)
+    {
+        for (i = 0; i < TAM - 1; i++)
+        {
+            if(numeros[i] > numeros[i+1])
+            {
+                aux = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = aux;
+            }
+        }
+    }
+
+    printf("\nElementos com ordenalção BubbleSort:\n");
+    for(i = 0; i < TAM; i++)
+    {
+        printf("%3d", numeros[i]);
+    }
+    return 0;
+}
+
---
+++ EstruturaDeDados/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int main()
+{
+    printf("Novo repositorio cambada!\n");
+    return 0;
+}
+*/
