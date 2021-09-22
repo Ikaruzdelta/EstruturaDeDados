b30eb77f2d7b4cb517dcc03d648323210a1c18eb
Ikaruz <ikaruz.music@hotmail.com>
2021-09-21 23:27:13 -0300

    Desafio02

---
+++ ED02/Desafio02/balanceLine2021/balanceLine.h
@@ -0,0 +1,16 @@
+/*
+ * balanceLine.h
+ *
+ *  Modified on: Sep 12, 2021
+ *      Author: raphael oliveira
+ */
+
+
+#include <stdio.h> /* define FILE */
+#include <stdlib.h>
+
+void balanceLine(FILE *fMestre, FILE *fTransacao, FILE *fNovoMestre, FILE *fErro){
+	//TODO
+	//implementar o algoritmo do Balance Line
+}
+
